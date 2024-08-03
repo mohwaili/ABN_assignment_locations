b5a7ea727ecc0628cf974c9d339ef5e1158847e4
mohwaili <mohammed.alwaili@gmail.com>
2024-08-03 18:24:37 +0200

    Initial Commit

---
+++ Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Mohammed Alwaili on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

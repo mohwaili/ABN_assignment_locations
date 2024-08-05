84c0e7707b0bfeda9231331ac3eb2a0662c45bde
mohwaili <mohammed.alwaili@gmail.com>
2024-08-05 21:18:59 +0200

    create a new test plan to test RTL languages such as Arabic or Hebrew

---
+++ Locations/Features/Tests/LocationsRTLSnapshotTests.swift
@@ -0,0 +1,181 @@
+//
+//  LocationsRTLSnapshotTests.swift
+//  LocationsTests
+//
+//  Created by Mohammed Alwaili on 05/08/2024.
+//
+
+import XCTest
+import SnapshotTesting
+import SwiftUI
+@testable import Locations
+
+final class LocationsRTLSnapshotTests: XCTestCase {
+    
+//     to re-record the snapshots
+//    override func invokeTest() {
+//        withSnapshotTesting(record: .all) {
+//            super.invokeTest()
+//        }
+//    }
+    
+    @MainActor
+    func test_errorState() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                error: NSError(
+                    domain: "LocationsSnapshotTests",
+                    code: 0
+                )
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(layoutDirection: .rightToLeft)
+            )
+        )
+    }
+    
+    @MainActor
+    func test_errorState_darkMode() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                error: NSError(
+                    domain: "LocationsSnapshotTests",
+                    code: 0
+                )
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(mutations: { mutableTraits in
+                    mutableTraits.userInterfaceStyle = .dark
+                    mutableTraits.layoutDirection = .rightToLeft
+                })
+            )
+        )
+    }
+    
+    @MainActor
+    func test_errorState_extraExtraExtraLarge() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                error: NSError(
+                    domain: "LocationsSnapshotTests",
+                    code: 0
+                )
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(mutations: { mutableTraits in
+                    mutableTraits.preferredContentSizeCategory = .extraExtraExtraLarge
+                    mutableTraits.layoutDirection = .rightToLeft
+                })
+            )
+        )
+    }
+    
+    @MainActor
+    func test_dataState() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                locations: [
+                    .amsterdam,
+                    .mumbai,
+                    .unknown
+                ]
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(layoutDirection: .rightToLeft)
+            )
+        )
+    }
+    
+    @MainActor
+    func test_dataState_darkMode() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                locations: [
+                    .amsterdam,
+                    .mumbai,
+                    .unknown
+                ]
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(mutations: { mutableTraits in
+                    mutableTraits.userInterfaceStyle = .dark
+                    mutableTraits.layoutDirection = .rightToLeft
+                })
+            )
+        )
+    }
+    
+    @MainActor
+    func test_dataState_extraExtraExtraLarge() async {
+        let view = LocationsView(
+            viewModel: await makeViewModel(
+                locations: [
+                    .amsterdam,
+                    .mumbai,
+                    .unknown
+                ]
+            )
+        )
+        assertSnapshot(
+            of: host(view),
+            as: .image(
+                on: .iPhoneSe,
+                traits: .init(mutations: { mutableTraits in
+                    mutableTraits.preferredContentSizeCategory = .extraExtraExtraLarge
+                    mutableTraits.layoutDirection = .rightToLeft
+                })
+            )
+        )
+    }
+    
+    @MainActor
+    func makeViewModel(
+        error: Error? = nil,
+        locations: [Location] = []
+    ) async -> LocationsViewModel {
+        let service = LocationsServiceMock()
+        service.fetchError = error
+        service.fetchReturnValue = locations
+        let viewModel = LocationsViewModel(
+            locationsService: service,
+            coordinator: CoordinatorMock(),
+            appInstalledChecker: { _ in false }
+        )
+        await viewModel.onLoad()
+        return viewModel
+    }
+}
+
+fileprivate class LocationsServiceMock: LocationsService {
+    
+    var fetchError: Error?
+    var fetchReturnValue: [Location] = []
+    func fetch() async throws -> [Location] {
+        if let fetchError {
+            throw fetchError
+        }
+        return fetchReturnValue
+    }
+}
+
+fileprivate class CoordinatorMock: Coordinator { }
